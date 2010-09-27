b64c2b55765691dfb3a38774cdfaf027b15acb14
lucazamador <lucazamador@gmail.com>
2010-09-27 00:38:36 -0300

    groupId renamed. added rules with activation group project initial structure

---
+++ chapter01/rules-with-activation-group/src/main/java/drools/cookbook/chapter01/ServerEvent.java
@@ -0,0 +1,46 @@
+package drools.cookbook.chapter01;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Lucas Amador
+ *
+ */
+public class ServerEvent {
+
+    private Server server;
+    private Date time;
+    private ServerStatus status;
+
+    public ServerEvent(Server server, Date time, ServerStatus status) {
+        this.server = server;
+        this.time = time;
+        this.setStatus(status);
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public void setStatus(ServerStatus status) {
+        this.status = status;
+    }
+
+    public ServerStatus getStatus() {
+        return status;
+    }
+
+}
