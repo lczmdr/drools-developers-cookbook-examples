adceff9fc0c5450838e81b700021f03cd06bc594
lucazamador <lucazamador@gmail.com>
2011-03-03 11:09:46 -0300

    chapter 5. pseudo clock example finished

---
+++ chapter05/pseudo-clock/src/main/java/drools/cookbook/helper/EmergencySystem.java
@@ -0,0 +1,17 @@
+package drools.cookbook.helper;
+
+public class EmergencySystem {
+
+    public static void sendFireTrucks() {
+        System.out.println("-> Fire trucks sended");
+    }
+
+    public static void sendAmbulances() {
+        System.out.println("-> Ambulances sended");
+    }
+
+    public static void cleanRunways() {
+        System.out.println("-> Cleaning runways in progress");
+    }
+
+}
