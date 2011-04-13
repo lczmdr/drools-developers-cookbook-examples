7e1827fa121a916136621144b70359b40171f74f
Lucas Amador <lucazamador@gmail.com>
2011-04-13 02:35:23 -0300

    chapter 07. spring integration example finished

---
+++ chapter07/spring-integration/src/test/java/drools/cookbook/chapter07/SpringIntegrationTest.java
@@ -0,0 +1,35 @@
+package drools.cookbook.chapter07;
+
+import junit.framework.Assert;
+
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author Lucas Amador
+ * 
+ */
+public class SpringIntegrationTest {
+
+    @Test
+    public void integration() {
+        
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("drools/cookbook/chapter07/context.xml");
+        applicationContext.start();
+
+        StatefulKnowledgeSession ksession1 = (StatefulKnowledgeSession) applicationContext.getBean("ksession1");
+
+        Assert.assertNotNull(ksession1);
+
+        Server debianServer = new Server("debian-1", 4, 2048, 250, 0);
+
+        ksession1.insert(debianServer);
+
+        Assert.assertEquals(1, ksession1.getFactCount());
+
+        applicationContext.stop();
+    }
+
+}
