5940b74202f2f2d5db8d06d4e278a315e7d5c1fd
Lucas Amador <lucazamador@gmail.com>
2011-04-14 03:16:54 -0300

    apache camel integration example

---
+++ chapter07/apache-camel-integration/src/test/java/drools/cookbook/chapter07/CamelIntegrationTest.java
@@ -0,0 +1,115 @@
+package drools.cookbook.chapter07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.jndi.JndiContext;
+import org.drools.KnowledgeBase;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.command.BatchExecutionCommand;
+import org.drools.command.CommandFactory;
+import org.drools.command.impl.GenericCommand;
+import org.drools.command.runtime.rule.FireAllRulesCommand;
+import org.drools.command.runtime.rule.InsertObjectCommand;
+import org.drools.grid.GridNode;
+import org.drools.grid.impl.GridImpl;
+import org.drools.grid.service.directory.WhitePages;
+import org.drools.grid.service.directory.impl.WhitePagesImpl;
+import org.drools.io.impl.ClassPathResource;
+import org.drools.runtime.ExecutionResults;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Lucas Amador
+ * 
+ */
+public class CamelIntegrationTest {
+
+    @Test
+    public void integration() throws Exception {
+
+        CamelContext camelContext = createCamelContext();
+        camelContext.start();
+
+        Server debianServer = new Server("debian", 2, 2048, 2048, 0);
+        Server winServer = new Server("win", 2, 1024, 250, 0);
+
+        Virtualization virtualization = new Virtualization("dev", "debian", 512, 30);
+
+        InsertObjectCommand insertServerCommand = new InsertObjectCommand();
+        insertServerCommand.setObject(debianServer);
+        insertServerCommand.setOutIdentifier("debian-server");
+
+        InsertObjectCommand insertBadServerCommand = new InsertObjectCommand();
+        insertBadServerCommand.setObject(winServer);
+        insertBadServerCommand.setOutIdentifier("win-server");
+
+        InsertObjectCommand insertVirtualizationCommand = new InsertObjectCommand();
+        insertVirtualizationCommand.setObject(virtualization);
+        insertVirtualizationCommand.setOutIdentifier("dev-virtualization");
+
+        FireAllRulesCommand fireAllRulesCommand = new FireAllRulesCommand();
+        fireAllRulesCommand.setOutIdentifier("executed-rules");
+
+        List<GenericCommand<?>> commands = new ArrayList<GenericCommand<?>>();
+        commands.add(insertServerCommand);
+        commands.add(insertBadServerCommand);
+        commands.add(insertVirtualizationCommand);
+        commands.add(fireAllRulesCommand);
+        BatchExecutionCommand batchExecutionCommand = CommandFactory.newBatchExecution(commands, "ksession1");
+
+        ProducerTemplate template = camelContext.createProducerTemplate();
+        ExecutionResults response = (ExecutionResults) template.requestBody("direct:test-with-session",
+                batchExecutionCommand);
+        Assert.assertNotNull(response);
+
+        Assert.assertNotNull(response.getFactHandle("debian-server"));
+
+        Assert.assertEquals(2, response.getValue("executed-rules"));
+
+    }
+
+    private CamelContext createCamelContext() throws Exception, NamingException {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(new ClassPathResource("rules.drl", getClass()), ResourceType.DRL);
+
+        if (kbuilder.hasErrors()) {
+            if (kbuilder.getErrors().size() > 0) {
+                for (KnowledgeBuilderError kerror : kbuilder.getErrors()) {
+                    System.err.println(kerror);
+                }
+            }
+        }
+        KnowledgeBase kbase = kbuilder.newKnowledgeBase();
+        StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+
+        GridImpl grid = new GridImpl();
+        grid.addService(WhitePages.class, new WhitePagesImpl());
+        GridNode node = grid.createGridNode("node");
+        Context context = new JndiContext();
+        context.bind("node", node);
+        node.set("ksession1", ksession);
+        CamelContext camelContext = new DefaultCamelContext(context);
+        RouteBuilder rb = new RouteBuilder() {
+            public void configure() throws Exception {
+                from("direct:test-with-session").to("drools://node/ksession1");
+            }
+        };
+        camelContext.addRoutes(rb);
+        return camelContext;
+    }
+
+}
