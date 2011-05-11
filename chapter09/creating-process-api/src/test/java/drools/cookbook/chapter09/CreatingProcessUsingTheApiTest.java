c9619eec6b0f392df6de9a7dde7d464e7d167d3d
Lucas Amador <lucazamador@gmail.com>
2011-05-11 02:26:00 -0300

    jbpm examples initial structure

---
+++ chapter09/creating-process-api/src/test/java/drools/cookbook/chapter09/CreatingProcessUsingTheApiTest.java
@@ -0,0 +1,17 @@
+package drools.cookbook.chapter09;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Lucas Amador
+ * 
+ */
+public class CreatingProcessUsingTheApiTest {
+
+	@Test
+	public void programaticallyIntegration() throws Exception {
+
+	}
+
+}
