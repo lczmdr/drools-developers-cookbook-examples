ae0f0d2a230df2bef2f578e8a79ae5af926a4046
Lucas Amador <lucazamador@gmail.com>
2011-08-19 12:09:00 -0300

    chapter 9 examples. creating process using the api. human task example.

--- chapter09/creating-process-using-the-api/src/test/java/drools/cookbook/chapter09/CreatingProcessUsingTheApiTest.java
+++ chapter09/creating-process-using-the-api/src/test/java/drools/cookbook/chapter09/CreatingProcessUsingTheApiTest.java
@@ -3,6 +3,7 @@ package drools.cookbook.chapter09;
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
 import org.drools.common.AbstractRuleBase;
+import org.drools.event.DebugProcessEventListener;
 import org.drools.impl.InternalKnowledgeBase;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessContext;
@@ -16,6 +17,7 @@ import org.jbpm.workflow.core.impl.DroolsConsequenceAction;
 import org.jbpm.workflow.core.node.ActionNode;
 import org.jbpm.workflow.core.node.EndNode;
 import org.jbpm.workflow.core.node.EventNode;
+import org.jbpm.workflow.core.node.Join;
 import org.jbpm.workflow.core.node.StartNode;
 import org.junit.Assert;
 import org.junit.Test;
@@ -71,16 +73,14 @@ public class CreatingProcessUsingTheApiTest {
     @Test
     public void newProcessWithScriptTask() {
         String processId = "processWithScriptTask";
+        String eventType = "eventType";
 
         StartNode startNode = new StartNode();
         startNode.setName("Start");
         startNode.setId(1);
-        EndNode endNode = new EndNode();
-        endNode.setName("EndNode");
-        endNode.setId(2);
 
         ActionNode actionNode = new ActionNode();
-        actionNode.setId(3);
+        actionNode.setId(2);
         DroolsConsequenceAction insertAction = new DroolsConsequenceAction("java", null);
         insertAction.setMetaData("Action", new Action() {
 
@@ -91,20 +91,44 @@ public class CreatingProcessUsingTheApiTest {
         });
         actionNode.setAction(insertAction);
 
+        Join joinNode = new Join();
+        joinNode.setId(3);
+        joinNode.setName("Waiting signal");
+        joinNode.setType(Join.TYPE_AND);
+
+        EventNode eventNode = new EventNode();
+        eventNode.setName("EventNode");
+        eventNode.setId(4);
+        eventNode.setScope("external");
+        EventTypeFilter eventFilter = new EventTypeFilter();
+        eventFilter.setType(eventType);
+        eventNode.addEventFilter(eventFilter);
+
+        EndNode endNode = new EndNode();
+        endNode.setName("EndNode");
+        endNode.setId(5);
+
         connect(startNode, actionNode);
-        connect(actionNode, endNode);
+        connect(actionNode, joinNode);
+        connect(eventNode, joinNode);
+        connect(joinNode, endNode);
 
         RuleFlowProcess process = new RuleFlowProcess();
         process.setId(processId);
         process.addNode(startNode);
         process.addNode(actionNode);
+        process.addNode(joinNode);
+        process.addNode(eventNode);
         process.addNode(endNode);
 
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
         ((AbstractRuleBase) ((InternalKnowledgeBase) kbase).getRuleBase()).addProcess(process);
         StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+        ksession.addEventListener(new DebugProcessEventListener());
         ProcessInstance processInstance = ksession.startProcess(processId);
         Assert.assertNotNull(processInstance);
+        Assert.assertEquals(ProcessInstance.STATE_ACTIVE, processInstance.getState());
+        ksession.signalEvent(eventType, null, processInstance.getId());
         Assert.assertEquals(ProcessInstance.STATE_COMPLETED, processInstance.getState());
     }
 
