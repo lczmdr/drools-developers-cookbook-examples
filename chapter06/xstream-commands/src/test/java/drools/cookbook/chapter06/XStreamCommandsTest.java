5d4ff01d727f69a3de8bd095f0a10b65c44f9b92
Lucas Amador <lucazamador@gmail.com>
2011-10-27 22:03:03 -0300

    dependencies updated

--- chapter06/xstream-commands/src/test/java/drools/cookbook/chapter06/XStreamCommandsTest.java
+++ chapter06/xstream-commands/src/test/java/drools/cookbook/chapter06/XStreamCommandsTest.java
@@ -1,10 +1,13 @@
 package drools.cookbook.chapter06;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Assert;
 
-import org.drools.command.runtime.BatchExecutionCommandImpl;
-import org.drools.command.runtime.rule.FireAllRulesCommand;
-import org.drools.command.runtime.rule.InsertObjectCommand;
+import org.drools.command.BatchExecutionCommand;
+import org.drools.command.Command;
+import org.drools.command.CommandFactory;
 import org.drools.runtime.help.BatchExecutionHelper;
 import org.junit.Test;
 
@@ -16,16 +19,51 @@ import org.junit.Test;
 public class XStreamCommandsTest {
 
     @Test
-    public void xstreamCommandsTest() throws InterruptedException {
+    public void xstreamCommandsFactoryTest() throws InterruptedException {
 
         Server server1 = new Server("windows-nt", 1, 2048, 2048, 3);
 
-        BatchExecutionCommandImpl batchExecutionCommand = new BatchExecutionCommandImpl();
-        batchExecutionCommand.setLookup("ksession1");
-        InsertObjectCommand insertObjectCommand = new InsertObjectCommand(server1);
-        FireAllRulesCommand fireAllRulesCommand = new FireAllRulesCommand();
-        batchExecutionCommand.getCommands().add(insertObjectCommand);
-        batchExecutionCommand.getCommands().add(fireAllRulesCommand);
+        List<Command> commands = new ArrayList<Command>();
+        BatchExecutionCommand batchExecutionCommand = CommandFactory.newBatchExecution(commands, "ksession1");
+
+        Command insertObjectCommand = CommandFactory.newInsert(server1);
+        Command fireAllRulesCommand = CommandFactory.newFireAllRules();
+        commands.add(insertObjectCommand);
+        commands.add(fireAllRulesCommand);
+
+        String result = BatchExecutionHelper.newXStreamMarshaller().toXML(batchExecutionCommand);
+
+        String expected = "<batch-execution lookup=\"ksession1\">\n";
+        expected += "  <insert>\n";
+        expected += "    <drools.cookbook.chapter06.Server>\n";
+        expected += "      <name>windows-nt</name>\n";
+        expected += "      <processors>1</processors>\n";
+        expected += "      <memory>2048</memory>\n";
+        expected += "      <diskSpace>2048</diskSpace>\n";
+        expected += "      <virtualizations/>\n";
+        expected += "      <cpuUsage>3</cpuUsage>\n";
+        expected += "      <online>false</online>\n";
+        expected += "    </drools.cookbook.chapter06.Server>\n";
+        expected += "  </insert>\n";
+        expected += "  <fire-all-rules/>\n";
+        expected += "</batch-execution>";
+
+        Assert.assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void xstreamCommandsFactoryTest2() throws InterruptedException {
+
+        Server server1 = new Server("windows-nt", 1, 2048, 2048, 3);
+
+        List<Command> commands = new ArrayList<Command>();
+        BatchExecutionCommand batchExecutionCommand = CommandFactory.newBatchExecution(commands, "ksession1");
+
+        Command insertObjectCommand = CommandFactory.newInsert(server1, "server-nt");
+        Command fireAllRulesCommand = CommandFactory.newFireAllRules();
+        commands.add(insertObjectCommand);
+        commands.add(fireAllRulesCommand);
 
         String result = BatchExecutionHelper.newXStreamMarshaller().toXML(batchExecutionCommand);
 
