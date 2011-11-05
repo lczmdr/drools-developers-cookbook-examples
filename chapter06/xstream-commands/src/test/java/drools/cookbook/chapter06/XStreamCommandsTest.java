34c3bd429e18c530623b6d0432c37d7b7fbb42c1
Lucas Amador <lucazamador@gmail.com>
2011-11-05 11:19:28 -0300

    chapter 6 unsused xstream removed

--- chapter06/xstream-commands/src/test/java/drools/cookbook/chapter06/XStreamCommandsTest.java
+++ chapter06/xstream-commands/src/test/java/drools/cookbook/chapter06/XStreamCommandsTest.java
@@ -52,38 +52,4 @@ public class XStreamCommandsTest {
 
     }
 
-    @Test
-    public void xstreamCommandsFactoryTest2() throws InterruptedException {
-
-        Server server1 = new Server("windows-nt", 1, 2048, 2048, 3);
-
-        List<Command> commands = new ArrayList<Command>();
-        BatchExecutionCommand batchExecutionCommand = CommandFactory.newBatchExecution(commands, "ksession1");
-
-        Command insertObjectCommand = CommandFactory.newInsert(server1, "server-nt");
-        Command fireAllRulesCommand = CommandFactory.newFireAllRules();
-        commands.add(insertObjectCommand);
-        commands.add(fireAllRulesCommand);
-
-        String result = BatchExecutionHelper.newXStreamMarshaller().toXML(batchExecutionCommand);
-
-        String expected = "<batch-execution lookup=\"ksession1\">\n";
-        expected += "  <insert>\n";
-        expected += "    <drools.cookbook.chapter06.Server>\n";
-        expected += "      <name>windows-nt</name>\n";
-        expected += "      <processors>1</processors>\n";
-        expected += "      <memory>2048</memory>\n";
-        expected += "      <diskSpace>2048</diskSpace>\n";
-        expected += "      <virtualizations/>\n";
-        expected += "      <cpuUsage>3</cpuUsage>\n";
-        expected += "      <online>false</online>\n";
-        expected += "    </drools.cookbook.chapter06.Server>\n";
-        expected += "  </insert>\n";
-        expected += "  <fire-all-rules/>\n";
-        expected += "</batch-execution>";
-
-        Assert.assertEquals(expected, result);
-
-    }
-
 }
